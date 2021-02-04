e4afd94547fde020709dbd4e803f5b883a1fcf3a
josharms00 <josharms00@gmail.com>
2021-02-04 15:20:37 -0500

    Added scripts for the freeze ray.

---
+++ Assets/Scripts/Devices/FreezeRay/PickUp.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUp : MonoBehaviour
+{
+    public Transform dest;
+
+    public Transform prefab;
+
+    public Text pickupTxt;
+
+    Transform t;
+
+    void OnTriggerStay(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            pickupTxt.enabled = true;
+
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                pickupTxt.enabled = false;
+                t = Instantiate(prefab, dest.position, prefab.rotation);
+                t.parent = GameObject.Find("GunHolder").transform;
+                t.rotation = new Quaternion(0, 0, 0, 0);
+                Destroy(gameObject);
+            }
+            
+        }
+    }
+
+    void OnTriggerExit()
+    {
+        pickupTxt.enabled = false;
+    }
+}
---
+++ Assets/Scripts/Devices/FreezeRay/ShootIce.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootIce : MonoBehaviour
+{
+    RaycastHit hit;
+
+    int layerMask;
+
+    ParticleSystem ice;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        layerMask = LayerMask.GetMask("Ground");
+        ice = GetComponentInChildren<ParticleSystem> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        {
+            if(Input.GetButton("Fire1"))
+            {
+                ice.Play();
+            }
+            
+            if(ice.isPlaying)
+            {
+                ice.Stop();
+            }
+        }
+
+        if(ice.isPlaying)
+        {
+            ice.Stop();
+        }
+    }
+}
