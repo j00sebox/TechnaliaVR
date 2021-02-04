d8ffbd76c7140168ab98fbe6fccc779e46347ccb
josharms00 <josharms00@gmail.com>
2021-02-03 19:52:00 -0500

    Added player character controller.

---
+++ Assets/Scripts/Player/LookAround.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAround : MonoBehaviour
+{
+    public float sensitivity = 100f;
+
+    public Transform body;
+
+    float xRot = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+
+        body.Rotate(Vector3.up * mouseX);
+    }
+}
