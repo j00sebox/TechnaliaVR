2e69d47554704a9c299990abd9e30f659f7ae00d
josharms00 <josharms00@gmail.com>
2021-02-04 11:38:06 -0500

    Player can now run.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -9,10 +9,14 @@ public class PlayerMovement : MonoBehaviour
 
     Animator anim;
 
-    public float speed = 12f;
+    public float walkSpeed = 9f;
+    public float sprintSpeed = 14f;
     public float gravity = -9.81f;
-
     public float jumpHeight = 3f;
+
+    float speed;
+
+
 
     Vector3 velocity;
 
@@ -34,9 +38,30 @@ public class PlayerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        if(z != 0)
+        {
+            if(Input.GetKey(KeyCode.LeftShift))
+            {
+                anim.SetBool("IsWalking", false);
+                anim.SetBool("IsRunning", true);
+                speed = sprintSpeed;
+            }
+            else
+            {
+                anim.SetBool("IsRunning", false);
+                anim.SetBool("IsWalking", true);
+                speed = walkSpeed;
+            }
+        }
+        else
+        {
+            anim.SetBool("IsWalking", false);
+        }
 
+        Vector3 move = transform.right * x + transform.forward * z;
+        
         controller.Move(move * speed * Time.deltaTime);
+        
 
         if(Input.GetButtonDown("Jump"))
         {
