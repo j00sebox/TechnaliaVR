5687f8c76a52ab4b4eb37db52a8df36ab7290e11
Josh <josharms00@gmail.com>
2021-03-13 16:38:01 -0500

    Added gitignore.

---
+++ Assets/Scripts/Misc/RespawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnManager : MonoBehaviour
+{
+   public Transform[] respawnPoints;
+
+   public Transform activeRespawn;
+
+   void Awake()
+   {
+       activeRespawn = respawnPoints[0];
+   }
+
+
+    // switch respawn points when player makes it to certain point
+   void OnTriggerEnter(Collider col)
+   {
+       if(col.tag == "Player")
+       {
+           activeRespawn = respawnPoints[1];
+       }
+   }
+
+
+}
