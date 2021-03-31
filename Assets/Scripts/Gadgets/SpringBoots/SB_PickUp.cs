36e721c88b9e8fbbf81650cd584c8269b167dcb7
Jonathan Cockfield <35935763+JonathanCockfield@users.noreply.github.com>
2021-03-31 18:20:07 -0400

    visuals

--- Assets/Scripts/Gadgets/SpringBoots/SB_PickUp.cs
+++ Assets/Scripts/Gadgets/SpringBoots/SB_PickUp.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class SB_PickUp : PickUp
 {
+
+    public static bool isHoldingSpringBoots = false;
+
     public override void pick_up_action(Transform player) 
     {
         player.GetComponent<PlayerMovement> ().springBoots = true;
+        isHoldingSpringBoots = true;
+
     }
 }
