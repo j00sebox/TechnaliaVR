4baa1b71db8cba7d00b02c069c02f171bc91720a
Josh <josharms00@gmail.com>
2021-04-04 12:31:39 -0400

    Added fps counter.

---
+++ Assets/Scripts/Misc/FPSCounter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour
+{
+
+    Text display;
+
+    void Awake()
+    {
+        display = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float fps = 1 / Time.unscaledDeltaTime;
+        display.text = "FPS: " + fps;
+    }
+}
